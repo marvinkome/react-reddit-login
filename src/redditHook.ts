79ebbb6fcad3dc6dea686951bdf49965c750a60a
Marvin Kome <marvinkome@gmail.com>
2020-07-31 18:10:00 +0100

    init commit

---
+++ src/redditHook.ts
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import { toQuery } from "./helpers";
+import { PopupWindow } from "./popup-window";
+
+export type RedditLoginOptions = {
+  clientId: string;
+  scope: string;
+  redirectUri: string;
+  onSuccess: (data: any) => void;
+  onFailure?: (data: any) => void;
+};
+export function useRedditLogin(options: RedditLoginOptions) {
+  const query = toQuery({
+    client_id: options.clientId,
+    response_type: "token",
+    state: "ranDomsPacestAte",
+    redirect_uri: encodeURIComponent(options.redirectUri),
+    scope: options.scope,
+  });
+
+  const signIn = () => {
+    PopupWindow.open(
+      "reddit-oauth-auth",
+      `https://www.reddit.com/api/v1/authorize.compact?${query}`,
+      {
+        height: 800,
+        width: 600,
+      }
+    )
+      ?.then((data: any) => options.onSuccess(data))
+      .catch((err: any) => options.onFailure && options.onFailure(err));
+  };
+
+  return signIn;
+}
